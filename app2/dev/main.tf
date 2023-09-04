46dbf49cdd6d3cef29476db0871e9f322134f2f1
Moisés Jurado <moises003@gmail.com>
2023-09-04 13:41:24 -0500

    providers

--- app2/dev/main.tf
+++ app2/dev/main.tf
@@ -1,4 +1,14 @@
 # Define your infrastructure resources for the 'dev' environment here.
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
